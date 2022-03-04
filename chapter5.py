7fd8fad681a9236aa1286d6d13ee47112a8e80cd
Jim Turner <jaxbeachcft@gmail.com>
2022-03-04 14:36:56 -0500

    First Commit

---
+++ chapter5.py
@@ -0,0 +1,31 @@
+# if BOOLEAN OR BOOLEAN EXP:
+    # code block
+    # for it true
+# else:
+    # for the if false code block
+
+from operator import truediv
+
+
+height = 75
+
+# must be 5 feet tall to ride my ride
+# must be under 6 feet tall to r
+
+x = 5
+print("A spoon", end=" ")
+if x == 4 or x==5:
+    print("full of", end=" ")
+elif x>=4:
+    print("sugar", end=" ")
+else:
+    print("helps the medicine", end=" ")
+print("go down", end=" ")
+
+
+    
+
+
+
+
+
